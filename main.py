3c92f5fe6566ce47b3273e4db6efc5c19cfb414e
Vidyonisio <vidyonisio@gmail.com>
2022-08-05 16:49:15 -0300

    Atualização

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import time
 import datetime
 
 "Automatiza o preenchimento da curva de carga nas boletas do Thunder via Bot"
-"É necessário estar com a planilha com os imputs aberta e a boleta aberta com o campo inicial selecionado na 3a janela"
+
+
+print("IMPORTANTE: É necessário estar com a planilha com os imputs aberta e a boleta aberta com o campo inicial selecionado na 3a janela e todos os anos já abertos")
 
 cab_ano = input('É cabeça de ano? [S/N]: ')
 date_entry1 = input('Coloque a Data inicial no formato: MM/YYYY: ')
@@ -28,10 +30,10 @@ print(meses)
 print(date1)
 print(date2)
 
-#print(pyautogui.position())
-#pyautogui.moveTo(3500,10)
+"print(pyautogui.position())" #Comandos auxiliares não utilizados
+"pyautogui.moveTo(3500,10)"
 
-def it_duplo_tab():
+def it_duplo_tab():  #Função de copiar e colar com duplo tab
     pyautogui.keyDown('alt')
     time.sleep(.2)
     pyautogui.press('tab')
@@ -49,7 +51,7 @@ def it_duplo_tab():
     pyautogui.hotkey('ctrlleft', 'v')
 
 
-def it_mono_tab():
+def it_mono_tab():  #Função de copiar e colar com um tab
     pyautogui.keyDown('alt')
     time.sleep(.2)
     pyautogui.press('tab')
@@ -66,7 +68,7 @@ def it_mono_tab():
     pyautogui.hotkey('ctrlleft', 'v')
 
 
-pyautogui.click()
+pyautogui.click()        #Rodada inicial de copiar do campo com mouse no excel e colar no primeiro campo pre-selecionado do thunders
 time.sleep(0.4)
 pyautogui.click()
 #print(pyautogui.KEYBOARD_KEYS)
@@ -81,23 +83,33 @@ pyautogui.keyUp('alt')
 pyautogui.hotkey('ctrlleft', 'v')
 
 if cab_ano == 'S':
-    it_duplo_tab()
+    it_duplo_tab()    #Iteração dupla inicial por ser cabeça
 
     if year2 > year1:
         i = 0
-        while i < (12 - month1):
+        while i < (12 - month1):   #Peenchimento do primeiro ano até o final
             it_mono_tab()
             i += 1
 
-        it_duplo_tab()
-        if meses > 24:
-            print("nada")
-        else:
+        it_duplo_tab()    #Roda o segundo duplo tab pra entrar no ano seguinte
+        if meses > 121 - month1:
+            print("caiu 121")
+        elif meses > 25 - month1:
+            i = 1
+            while i < 12:
+                it_mono_tab()
+                i += 1
+            it_duplo_tab()
+            i = 1
+            while i < month2 - 1:
+                it_mono_tab()
+                i += 1
+        else:           #Caso em que o segundo ano já é o último, itera até finalizar ele
             i = 1
             while i < month2-1:
                 it_mono_tab()
                 i += 1
-    else:
+    else:  #Caso em que não pula pra outro ano
         i = 1
         while i < (month2-month1):
             it_mono_tab()
